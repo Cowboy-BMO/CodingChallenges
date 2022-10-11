57d0cf9416e47acdd7640aed918f387cb64e6a91
Cowboy-BMO <89423467+Cowboy-BMO@users.noreply.github.com>
2022-10-11 10:39:00 -0600

    minimaxSum for hackerrank done in java 8

---
+++ minimaxSum.java
@@ -0,0 +1,52 @@
+import java.io.*;
+import java.math.*;
+import java.security.*;
+import java.text.*;
+import java.util.*;
+import java.util.concurrent.*;
+import java.util.function.*;
+import java.util.regex.*;
+import java.util.stream.*;
+import static java.util.stream.Collectors.joining;
+import static java.util.stream.Collectors.toList;
+
+class Result {
+
+    /*
+     * Complete the 'miniMaxSum' function below.
+     *
+     * The function accepts INTEGER_ARRAY arr as parameter.
+     */
+
+    public static void miniMaxSum(List<Integer> arr) {
+    Long minSum = Long.MAX_VALUE;
+    Long maxSum = Long.MIN_VALUE;
+    Long totalSum = 0L;
+    //loop through all find the 
+    for(long var: arr){
+        totalSum += var;
+        if( var < minSum){
+            minSum = var;
+        }
+        if(var > maxSum){
+            maxSum = var;
+        }
+    }
+    System.out.println((totalSum-maxSum) + " " + (totalSum-minSum));
+    }
+
+}
+
+public class Solution {
+    public static void main(String[] args) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+
+        List<Integer> arr = Stream.of(bufferedReader.readLine().replaceAll("\\s+$", "").split(" "))
+            .map(Integer::parseInt)
+            .collect(toList());
+
+        Result.miniMaxSum(arr);
+
+        bufferedReader.close();
+    }
+}
